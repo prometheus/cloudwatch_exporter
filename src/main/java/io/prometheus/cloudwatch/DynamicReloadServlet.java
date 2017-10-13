5df8509736f761918cf357ed71eae93fb2b66ad0
Joe Martin <jwmartin83@gmail.com>
2017-10-13 13:37:33 -0400

    add get endpoint

--- src/main/java/io/prometheus/cloudwatch/DynamicReloadServlet.java
+++ src/main/java/io/prometheus/cloudwatch/DynamicReloadServlet.java
@@ -11,6 +11,10 @@ import java.util.logging.Logger;
 public class DynamicReloadServlet extends HttpServlet {
     private static final Logger LOGGER = Logger.getLogger(CloudWatchCollector.class.getName());
 
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setStatus(405);
+        resp.setContentType("text/plain");
+        resp.getWriter().print("Only POST requests allowed");
     }
 
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
