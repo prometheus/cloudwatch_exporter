4cb86df1e0cdb34f04dd612c0b7b410dfa3708a6
Alok Kumar Singh <alok.singh@practo.com>
2017-12-19 10:38:47 +0000

    Deploy to kubernetes

---
+++ deploy/deploy.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+GIT_UNTRACKED_FILES=`git ls-files --others --exclude-standard | wc -l`
+GIT_UNSTAGED_DIFF=`git diff --no-ext-diff | wc -l`
+GIT_STAGED_DIFF=`git diff-index --cached HEAD -- | wc -l`
+NAMESPACE='monitoring'
+ORG='practodev'
+IMAGE_NAME='cloudwatch-exporter'
+IMAGE_SECRET='dev-secret-docker'
+DEPLOYMENT_YAML='deploy/deployment.yaml'
+
+if [[ $GIT_UNTRACKED_FILES -ne 0 || ${GIT_UNSTAGED_DIFF} -ne 0 || ${GIT_STAGED_DIFF} -ne 0 ]]; then
+    echo "[ERROR]: Deployment not started as there are local changes"
+    exit 1
+fi
+
+echo "Pulling latest code"
+git pull origin master
+
+echo "Building image"
+TAG=`git rev-parse HEAD`
+docker build -t ${IMAGE_NAME}:${TAG} .
+
+echo "Pushing new image"
+docker push ${IMAGE_NAME}:${TAG}
+
+echo "Creating new deployment yaml"
+cp deploy/deployment.sample.yaml ${DEPLOYMENT_YAML}
+sed -i "s#{{tag}}#${TAG}#g" ${DEPLOYMENT_YAML}
+sed -i "s#{{org}}#${ORG}#g" ${DEPLOYMENT_YAML}
+sed -i "s#{{aws_acess_key_id}}#${AWS_ACCESS_KEY_ID}#g" ${DEPLOYMENT_YAML}
+sed -i "s#{{aws_secret_access_key}}#${AWS_SECRET_ACCESS_KEY}#g" ${DEPLOYMENT_YAML}
+sed -i "s#{{image_secret}}#${IMAGE_SECRET}g" ${DEPLOYMENT_YAML}
+
+echo "Rolling update first status"
+kubectl apply -f ${DEPLOYMENT_YAML}
+kubectl get pods -n ${NAMESPACE} | grep cloudwatch
+
+echo "Check rollout status using"
+echo kubectl get pods -n ${NAMESPACE} 
