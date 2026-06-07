8f0acbbf077fc00b07f3407e87825e10c024c101
dhoard <doug.hoard@gmail.com>
2026-06-07 17:50:31 -0400

    chore: Fixed copyright notice
    Signed-off-by: dhoard <doug.hoard@gmail.com>

--- scripts/stress-test.sh
+++ scripts/stress-test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright (c) 2026-present Douglas Hoard
+# Copyright (c) 2026 The Prometheus Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
